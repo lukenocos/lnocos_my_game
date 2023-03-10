103fbbdbd91e02160750f7562c803a6032dbc827
lukenocos <lukenocos.gmail.com>
2023-03-10 11:39:16 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# File created by Luke Nocos
+# Agenda:
+# GIT GITHUB
+# Build file and folder structures
+# Create Libraries
+
+# Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
+# Sources: 
+
+# import libs
+import pygame
+import random
+import os
+# import settings 
+from settings import *
+from sprites import *
+# from pygame.sprite import Sprite
+
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+def get_mouse_now():
+    x,y = pygame.mouse.get_pos()
+    return (x,y)
+
+
+# init pygame and create window
+pygame.init()
+# init sound mixer
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My first game...")
+clock = pygame.time.Clock() 
+
+all_sprites = pygame.sprite.Group()
+enemies = pygame.sprite.Group()
+player = Player()
+
+enemy1 = Mob(80, 80)
+
+
+all_sprites.add(player)
+all_sprites.add(enemy1)
+# all_sprites.add(testSprite)
+
+# game loop
+
+while RUNNING:
+    #  keep loop running at the right speed
+    clock.tick(FPS)
+    ### process input events section of game loop
+    for event in pygame.event.get():
+        # check for window closing
+        if event.type == pygame.QUIT:
+            RUNNING = False
+            # break
+    # print(get_mouse_now())
+    ### update section of game loop (if updates take longer the 1/30th of a second, you will get laaaaag...)
+    all_sprites.update()
+
+    blocks_hit_list = pygame.sprite.spritecollide(player, enemies, True)
+    for block in blocks_hit_list:
+        # print(enemies)
+        pass
+    ### draw and render section of game loop
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    # double buffering draws frames for entire screen
+    pygame.display.flip()
+    # pygame.display.update() -> only updates a portion of the screen
+# ends program when loops evaluates to false
+pygame.quit()
---
+++ sprites.py
@@ -0,0 +1,93 @@
+import pygame as pg
+
+from pygame.sprite import Sprite
+
+from settings import *
+
+vec = pg.math.Vector2
+
+# player class
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def input(self):
+        keystate = pg.key.get_pressed()
+
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+
+    # method that will keep square on screen
+    def inbounds(self):
+        if self.rect.x > WIDTH - 50:
+            self.pos.x = WIDTH - 25
+            self.vel.x = 0
+            print("i am off the right side of the screen...")
+        if self.rect.x < 0:
+            print("i am off the left side of the screen...")
+        if self.rect.y > HEIGHT:
+            print("i am off the bottom of the screen")
+        if self.rect.y < 0:
+            print("i am off the top of the screen...")
+            
+    def update(self):
+        self.inbounds()
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
+
+class Mob(Sprite):
+    def __init__(self,width,height):
+        Sprite.__init__(self)
+        self.width = width
+        self.height = height
+        self.image = pg.Surface((self.width,self.height))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+    
+    def inbounds(self):
+        if self.rect.x > WIDTH - 50:
+            self.pos.x = WIDTH - 25
+            self.vel.x = 0
+            print("i am off the right side of the screen...")
+        if self.rect.x < 0:
+            print("i am off the left side of the screen...")
+        if self.rect.y > HEIGHT:
+            print("i am off the bottom of the screen")
+        if self.rect.y < 0:
+            print("i am off the top of the screen...")
+
+    def behavior(self):
+        self.acc.y = -2
+
+
+    def update(self):
+        # self.inbounds()
+        
+        self.acc = self.vel * MOB_FRICTION
+        self.vel += self.acc
+        self.behavior()
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
