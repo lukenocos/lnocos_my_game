dbd80383749017a7548d321a4f4b9278031770c3
lukenocos <lukenocos.gmail.com>
2023-03-16 12:39:03 -0700

    added score and collision

--- main.py
+++ main.py
@@ -20,6 +20,14 @@ from sprites import *
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, "img")
 
+def draw_text(text, size, color, x, y):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    screen.blit(text_surface, text_rect)
+
 def get_mouse_now():
     x,y = pg.mouse.get_pos()
     return (x,y)
@@ -32,16 +40,21 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("My first game...")
 clock = pg.time.Clock() 
 
-
+# what are those
 all_sprites = pg.sprite.Group()
 enemies = pg.sprite.Group()
+
 player = Player()
 
 all_sprites.add(player)
 player.pos = (0,0)
+
+# creates a for loop that creates 20 mobs
+# 
 for i in range(0,20):
     # instantiate mobs
     m = Mob(randint(30,90), randint(30, 90), (255,0,0))
+    # add enemies to enemies and all_sprites...
     enemies.add(m)
     all_sprites.add(m)
 
@@ -62,13 +75,21 @@ while RUNNING:
     enemies.update()
     all_sprites.update()
 
-    blocks_hit_list = pg.sprite.spritecollide(player, enemies, False)
+    # if player hits enemies
+    blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
+    
     for block in blocks_hit_list:
-        # print(enemies)
+        print(block)
         pass
+    if blocks_hit_list:
+        SCORE += SCORE_INCREMENT
+        
+    
+
     ### draw and render section of game loop
     screen.fill(BLUE)
     all_sprites.draw(screen)
+    draw_text("Score: " + str(SCORE), 22, WHITE, WIDTH/2, HEIGHT/10)
     # double buffering draws frames for entire screen
     pg.display.flip()
     # pg.display.update() -> only updates a portion of the screen
